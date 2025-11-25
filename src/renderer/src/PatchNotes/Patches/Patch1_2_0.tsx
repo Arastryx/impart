d62a327781e85b3e485699a4d9063b79eabfc8ec
Arastryx <arastryx.dravoga@gmail.com>
2025-11-24 21:57:55 -0400

    Updated Patch Notes
    -Added all of the missing stuff from the patch notes

--- src/renderer/src/PatchNotes/Patches/Patch1_2_0.tsx
+++ src/renderer/src/PatchNotes/Patches/Patch1_2_0.tsx
@@ -91,17 +91,34 @@ export function Patch1_2_0({}: Patch1_2_0Props) {
           Added in-app patch notes (which is how you're seeing this right now!)
         </Typography>
         <Typography component="li">
+          Added the ability to disable auto-updating (in the settings under "Updates")
+        </Typography>
+        <Typography component="li">
           Added a Refresh (<RefreshIcon fontSize="inherit" sx={{ verticalAlign: 'middle' }} />)
           button which re-indexes all directories and loads any changed files
         </Typography>
-        <Typography component={'li'}>
+        <Typography variant="body2" component={'li'} sx={{ mt: 1 }}>
+          Updated almost every single package internally; Electron, React, Vite, MUI, you name it!
+        </Typography>
+        <Typography variant="body2" component={'li'}>
+          Fixed a bug where updating a directory's recursiveness didn't actually do anything
+        </Typography>
+        <Typography variant="body2" component={'li'}>
+          Fixed reported file changes when updating directory settings ("This will add X files and
+          remove X files") being slightly off
+        </Typography>
+        <Typography variant="body2" component={'li'}>
           Fixed the app becoming unresponsive when using the "Create Stack" button in the selection
           indicator (the little popup on the lower left corner of the screen)
         </Typography>
-        <Typography component={'li'}>
+        <Typography variant="body2" component={'li'}>
           Fixed not being able to add a folder if its name "contained" another folder that was
           already indexed and is recursive (<code>/my folder</code> vs{' '}
           <code>/my folder but cooler</code>)
+        </Typography>
+        <Typography variant="body2" component={'li'}>
+          The whole app now properly uses the correct fonts. IDK if there were any spots that used
+          the wrong font, but if there were, they're gone now
         </Typography>
       </ul>
     </Box>
