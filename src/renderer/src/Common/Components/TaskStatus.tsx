43175a8f05b99264f2a42fbf0b726de6d3412ee6
Arastryx <arastryx.dravoga@gmail.com>
2026-05-13 18:08:51 -0300

    Added "Cancel Tasks" button
    -On the indexing panel, added a "Cancel Tasks" button hidden behind a dropdown

--- src/renderer/src/Common/Components/TaskStatus.tsx
+++ src/renderer/src/Common/Components/TaskStatus.tsx
@@ -1,9 +1,26 @@
-import { Box, Card, CardContent, LinearProgress, Stack, Typography } from '@mui/material'
+import {
+  Box,
+  Card,
+  CardContent,
+  IconButton,
+  LinearProgress,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Stack,
+  Typography
+} from '@mui/material'
 import { useTaskStatus } from '@renderer/TaskStatusProvider'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import { useRef, useState } from 'react'
+import CancelIcon from '@mui/icons-material/Cancel'
 
 export interface TaskStatusProps {}
 
 export function TaskStatus({}: TaskStatusProps) {
+  const [showMenu, setShowMenu] = useState(false)
+  const anchorRef = useRef<HTMLButtonElement>(null)
   const { currentStep, stepCount, taskType, currentTask, taskCount } = useTaskStatus()
 
   const getTaskLabel = () => {
@@ -22,21 +39,43 @@ export function TaskStatus({}: TaskStatusProps) {
   }
 
   return (
-    <Stack gap={2}>
-      <Box>
-        <Typography variant="body2">
-          Step {Math.min(currentTask + 1, taskCount)}/{taskCount}
-        </Typography>
-        {taskCount != 0 && (
-          <LinearProgress value={(currentTask / taskCount) * 100} variant="determinate" />
-        )}
+    <Box position="relative">
+      <Stack gap={2} pt={1}>
+        <Box>
+          <Typography variant="body2">
+            Step {Math.min(currentTask + 1, taskCount)}/{taskCount}
+          </Typography>
+          {taskCount != 0 && (
+            <LinearProgress value={(currentTask / taskCount) * 100} variant="determinate" />
+          )}
+        </Box>
+        <Box>
+          <Typography variant="body2">{getTaskLabel()}</Typography>
+          {stepCount != 0 && (
+            <LinearProgress value={(currentStep / stepCount) * 100} variant="determinate" />
+          )}
+        </Box>
+      </Stack>
+      <Box position={'absolute'} top={-10} right={-10}>
+        <IconButton size="small" onClick={() => setShowMenu(true)} ref={anchorRef}>
+          <MoreVertIcon fontSize="inherit" />
+        </IconButton>
       </Box>
-      <Box>
-        <Typography variant="body2">{getTaskLabel()}</Typography>
-        {stepCount != 0 && (
-          <LinearProgress value={(currentStep / stepCount) * 100} variant="determinate" />
-        )}
-      </Box>
-    </Stack>
+      <Menu
+        open={showMenu}
+        onClose={() => setShowMenu(false)}
+        anchorEl={anchorRef.current}
+        slotProps={{ list: { dense: true } }}
+        sx={{ paddingX: '16px', '.MuiListItemIcon-root': { minWidth: '30px' } }}
+      >
+        <MenuItem>
+          <ListItemIcon>
+            <CancelIcon />
+          </ListItemIcon>
+          <ListItemText>Cancel Tasks</ListItemText>
+        </MenuItem>
+      </Menu>
+    </Box>
   )
 }
+
