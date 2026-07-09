d52fea0542e14837db6ae18f06108084928ad7d8
Arastryx <arastryx.dravoga@gmail.com>
2026-07-09 19:55:23 -0300

    Added Additional Empty Images
    -Added one new normal empty image, one silly empty image and one rare empty image

--- src/renderer/src/Common/Components/TaggableGrid/EmptyImages/index.ts
+++ src/renderer/src/Common/Components/TaggableGrid/EmptyImages/index.ts
@@ -3,6 +3,9 @@ import empty2 from './NoItemsFound2.png'
 import empty3 from './NoItemsFound3.png'
 import empty4 from './NoItemsFound4.png'
 import empty5 from './NoItemsFound5.png'
+import empty6 from './NoItemsFound6.png'
+import empty7 from './NoItemsFound7.png'
+import empty8 from './NoItemsFound8.jpg'
 import secret from './NoItemsFoundSECRET.png'
 
 interface EmptyImage {
@@ -17,5 +20,8 @@ export const emptyImages: EmptyImage[] = [
   { src: empty3 },
   { src: empty4 },
   { src: empty5, chance: 0.1, minViews: 10 },
+  { src: empty6, chance: 0.25, minViews: 10 },
+  { src: empty7 },
+  { src: empty8, chance: 0.1, minViews: 10 },
   { src: secret, chance: 0.01, minViews: 100 }
 ]
